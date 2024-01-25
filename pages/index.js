53811205c2371cd4e3ed269554aff59aa2c47c49
Chatchol <chatchol1973@gmail.com>
2024-01-25 21:58:47 +0700

    2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import How from "./compernent/post";
 import Three from "./compernent/threething";
 import Title from "./compernent/title";
+import Image from "next/image";
 export default function Home() {
   return (
     <div>
