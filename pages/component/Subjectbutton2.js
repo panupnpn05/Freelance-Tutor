80884f1629fea4deba5023a55912ca4cc9b2bb59
Paramat Rakthai <Paramet121@gmail.com>
2024-01-30 18:59:40 +0700

    ข้างล่างหน้าโฮม *เสร็จ

--- pages/component/Subjectbutton2.js
+++ pages/component/Subjectbutton2.js
@@ -3,8 +3,8 @@ export default function Subjectbutton2() {
   return (
     <>
     <div className="flex justify-row mb-8" style={{ marginLeft: '15px'}}>
-      <a style={{  display: 'inline-block', padding: '35px 48px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/math">ภาษาญี่ปุ่น</a>
-      <a style={{  marginLeft: '15px' ,display: 'inline-block', padding: '35px 63px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/science">ภาษาจีน</a>
+      <a style={{  display: 'inline-block', padding: '35px 48px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/japan">ภาษาญี่ปุ่น</a>
+      <a style={{  marginLeft: '15px' ,display: 'inline-block', padding: '35px 63px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/china">ภาษาจีน</a>
       </div>
     </>    
   );
--- pages/component/Subjectbutton3.js
+++ pages/component/Subjectbutton3.js
@@ -4,9 +4,9 @@ export default function Subjectbutton3() {
     <>
     <div className="flex justify-row ">
       <a style={{  marginLeft: '100px',textAlign: 'center',display: 'inline-block', padding: '30px 45px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/coding">Coding</a>
-      <a style={{ marginLeft: '65px',textAlign: 'center',display: 'inline-block', padding: '30px 45px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/thai">ชีววิทยา</a>
-      <a style={{  marginLeft: '90px',display: 'inline-block', padding: '30px 45px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/china">วิทยาศาสตร์</a>
-      <a style={{ marginRight: '100px',marginLeft: '15px',display: 'inline-block', padding: '30px 55px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/japan">คณิตศาสตร์</a>
+      <a style={{ marginLeft: '65px',textAlign: 'center',display: 'inline-block', padding: '30px 45px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/biology">ชีววิทยา</a>
+      <a style={{  marginLeft: '90px',display: 'inline-block', padding: '30px 45px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/science">วิทยาศาสตร์</a>
+      <a style={{ marginRight: '100px',marginLeft: '15px',display: 'inline-block', padding: '30px 55px', background: 'lightgray', color: 'black', textDecoration: 'none', fontSize: '18px' }} href="/math">คณิตศาสตร์</a>
       </div>
     </>    
   );
